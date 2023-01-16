05630236765f17a8fccf3ccd838d156f276e5b35
mowgly11 <83482871+mowgly11@users.noreply.github.com>
2023-01-17 00:06:47 +0100

    portfolio refactoring

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const loading = document.querySelector(".loading-screen");
+const hamBtn = document.getElementById("hamburger");
+
 function openSite(link) {
-    if(!link) return;
     window.open(`https://${link}`);
 }
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        loading.classList.add("hide");
+    }, 2000);
+});
+
+hamBtn.addEventListener("click", () => {
+    document.getElementById("bg").classList.toggle("showHam");
+    document.getElementById("navigation-links").classList.toggle("showHamNav");
+});
