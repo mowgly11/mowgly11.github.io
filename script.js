c73c2e217e5fcc534e37a12f7a467d220cb56241
mowgly11 <83482871+mowgly11@users.noreply.github.com>
2023-01-26 00:22:29 +0100

    animate on scroll

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const loading = document.querySelector(".loading-screen");
 const hamBtn = document.getElementById("hamburger");
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("showing");
+        }
+    })
+});
+
+hiddenElements.forEach(element => observer.observe(element));
 
 function openSite(link) {
     window.open(`https://${link}`);
 }
-
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        loading.classList.add("hide");
-    }, 2000);
-});
 
 hamBtn.addEventListener("click", () => {
     document.getElementById("bg").classList.toggle("showHam");
