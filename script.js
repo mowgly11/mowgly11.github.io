9b19d92874c9828655ee06bacaedb98f01acd92f
mowgly11 <83482871+mowgly11@users.noreply.github.com>
2023-01-29 03:58:50 +0100

    eye effect

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ hamBtn.addEventListener("click", () => {
     document.getElementById("navigation").classList.toggle("showNavigation");
     document.getElementById("hamburger").classList.toggle("animateBtn");
 });
+
+document.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    const face = document.getElementById("face");
+    const rekt = face.getBoundingClientRect();
+
+    const faceX = rekt.left + rekt.width / 2;
+    const faceY = rekt.top + rekt.height / 2;
+
+    const angle = calculateAngle(mouseX, mouseY, faceX, faceY);
+
+    const eyes = document.querySelectorAll(".eye");
+
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angle}deg)`
+    })
+});
+
+function calculateAngle(cx, cy, ex, ey) {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = rad * 180 / Math.PI;
+    return deg
+}
