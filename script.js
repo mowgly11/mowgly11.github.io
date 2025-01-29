082467e9fd238c9f0f31c05cc5c43732a612b206
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-29 18:00:02 +0100

    copy email when clicked feature

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let emailElement = document.getElementById("email");
+let copiedText = document.getElementById("copied-text");
+
 function takeToSite(site) {
     window.open(site);
+}
+
+let cooldown = false;
+function copyEmail() {
+    navigator.clipboard.writeText("oussamabouzalim4@gmail.com");
+    if (cooldown) return;
+    cooldown = true;
+    copiedText.style.opacity = 1;
+
+    setTimeout(() => {
+        copiedText.style.opacity = 0;
+        cooldown = false;
+    }, 2000);
 }
