6c3801e320a051bd00aec50563ce92347b950670
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-29 20:33:55 +0100

    now availability is set using the config.json file

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-let emailElement = document.getElementById("email");
-let copiedText = document.getElementById("copied-text");
+document.addEventListener("DOMContentLoaded", async() => {
+    const data = await fetch("./config.json").then(res => res.json());
+    let root = document.querySelector(':root');
+    let availableText = document.getElementById("status");
+
+    if(data.available) {
+        root.style.setProperty("--availability", 'green');
+        availableText.textContent = "Available";
+    } else {
+        root.style.setProperty("--availability", 'red');
+        availableText.textContent = "On Vacation";
+    }
+});
 
 function takeToSite(site) {
     window.open(site);
 }
 
 let cooldown = false;
+let emailElement = document.getElementById("email");
+let copiedText = document.getElementById("copied-text");
 function copyEmail() {
     navigator.clipboard.writeText("oussamabouzalim4@gmail.com");
     if (cooldown) return;
