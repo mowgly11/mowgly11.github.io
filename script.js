c9247a39b927f7075b90306d7e1371f0f3fd650a
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-06 20:27:04 +0100

    new website look

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", async() => {
+document.addEventListener("DOMContentLoaded", async () => {
     const data = await fetch("./config.json").then(res => res.json());
     let root = document.querySelector(':root');
     let availableText = document.getElementById("status");
 
-    if(data.available) {
+    if (data.available) {
         root.style.setProperty("--availability", 'green');
         availableText.textContent = "Available";
     } else {
@@ -29,4 +29,48 @@ function copyEmail() {
         copiedText.style.opacity = 0;
         cooldown = false;
     }, 2000);
+}
+
+document.getElementById("age").textContent = new Date().getFullYear() - 2005;
+
+let runnerImg = document.getElementById("runner-animation");
+let bookImg = document.getElementById("book-animation");
+
+let runnerImgs = [
+    "./imgs/runner-frame3.png",
+    "./imgs/runner-frame2.png",
+    "./imgs/runner-frame4.png",
+    "./imgs/runner-frame1.png",
+];
+
+let bookImgs = [
+    "./imgs/book-half.png",
+    "./imgs/book-open.png",
+];
+
+let runnerInterval;
+
+runnerImg.onmouseenter = function () {
+    let i = 0;
+    runnerInterval = setInterval(() => {
+        if (i === runnerImgs.length) i = 0;
+        runnerImg.setAttribute("src", runnerImgs[i]);
+        i++;
+    }, 300);
+}
+
+runnerImg.onmouseleave = function() {
+    clearInterval(runnerInterval);
+}
+
+bookImg.onmouseenter = async function () {
+    bookImg.setAttribute("src", "./imgs/book-half.png");
+    await new Promise(res => setTimeout(res, 300));
+    bookImg.setAttribute("src", "./imgs/book-open.png");
+}
+
+bookImg.onmouseleave = async function() {
+    bookImg.setAttribute("src", "./imgs/book-half.png");
+    await new Promise(res => setTimeout(res, 300));
+    bookImg.setAttribute("src", "./imgs/book-closed.png");
 }
